3f61e966e24986001c95c4a2aeb5eadf7f407c81
Steven Espejo <55455142+C0C045@users.noreply.github.com>
2024-07-31 13:18:36 -0500

    fix: planes crud desde MF

--- src/app/components/formulacion/tabla-formulacion.component.ts
+++ src/app/components/formulacion/tabla-formulacion.component.ts
@@ -85,7 +85,7 @@ export class TablaFormulacionComponent implements OnInit, AfterViewInit {
     this.CODIGO_TIPO_PROYECTO = await this.codigosService.getId('PLANES_CRUD', 'tipo-plan', 'PR_SP');
     print("1", this.CODIGO_TIPO_PROYECTO)
     this.request.get(environment.PLANES_CRUD, `tipo-plan?query=codigo_abreviacion:PR_SP,activo:true`).subscribe({
-      next: (data: DataRequest) => {
+      next: (data: any) => {
         if (data.Data[0]) {
           print("2", data.Data[0]._id);
         }
