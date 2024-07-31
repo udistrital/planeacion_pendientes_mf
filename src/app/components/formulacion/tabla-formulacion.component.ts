767d855f95e8cf1efa98aa16b3c63cd52d98324e
Steven Espejo <55455142+C0C045@users.noreply.github.com>
2024-07-31 13:29:40 -0500

    fix: prueba planes crud

--- src/app/components/formulacion/tabla-formulacion.component.ts
+++ src/app/components/formulacion/tabla-formulacion.component.ts
@@ -82,8 +82,8 @@ export class TablaFormulacionComponent implements OnInit, AfterViewInit {
   }
 
   async ngOnInit() {
-    this.CODIGO_TIPO_PROYECTO = await this.codigosService.getId('PLANES_CRUD', 'tipo-plan', 'PR_SP');
-    console.log("1", this.CODIGO_TIPO_PROYECTO)
+    // this.CODIGO_TIPO_PROYECTO = await this.codigosService.getId('PLANES_CRUD', 'tipo-plan', 'PR_SP');
+    // console.log("1", this.CODIGO_TIPO_PROYECTO)
     this.request.get(environment.PLANES_CRUD, `tipo-plan?query=codigo_abreviacion:PR_SP,activo:true`).subscribe({
       next: (data: any) => {
         if (data.Data[0]) {
